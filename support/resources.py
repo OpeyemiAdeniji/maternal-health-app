0a981d39b92d63d4d570b5754e5c2b66be1cbc10
Opeyemi Adeniji <adenijiopeyemi68@gmail.com>
2026-07-07 14:30:00 +0100

    feat(support): add country-aware support resources for five countries

--- support/resources.py
+++ support/resources.py
@@ -1,20 +1,72 @@
-IRISH_SUPPORT_RESOURCES = [
-    {
-        'name': 'HSE Perinatal Mental Health',
-        'description': 'HSE mental health services for women during pregnancy and after birth',
-        'url': 'https://www2.hse.ie/mental-health/services-support/perinatal/',
-        'phone': '1800 742 444',
-    },
-    {
-        'name': 'Nurture Health',
-        'description': 'Free perinatal mental health support for parents in Ireland',
-        'url': 'https://www.nurturehealth.ie',
-        'phone': '01 629 8891',
-    },
-    {
-        'name': 'Samaritans Ireland',
-        'description': 'Free confidential emotional support available 24 hours a day',
-        'url': 'https://www.samaritans.org/ireland',
-        'phone': '116 123',
-    },
-]
+SUPPORT_RESOURCES = {
+    'IRELAND': [
+        {
+            'name': 'HSE Perinatal Mental Health',
+            'description': 'HSE mental health services for women during pregnancy and after birth',
+            'url': 'https://www2.hse.ie/mental-health/services-support/perinatal/',
+            'phone': '1800 742 444',
+        },
+        {
+            'name': 'Nurture Health',
+            'description': 'Free perinatal mental health support for parents in Ireland',
+            'url': 'https://www.nurturehealth.ie',
+            'phone': '01 629 8891',
+        },
+        {
+            'name': 'Samaritans Ireland',
+            'description': 'Free confidential emotional support available 24 hours a day',
+            'url': 'https://www.samaritans.org/ireland',
+            'phone': '116 123',
+        },
+    ],
+    'UNITED_KINGDOM': [
+        {
+            'name': 'PANDAS Foundation',
+            'description': 'Support for perinatal mental illness for parents and families',
+            'url': 'https://pandasfoundation.org.uk',
+            'phone': '0808 1961 776',
+        },
+        {
+            'name': 'Samaritans UK',
+            'description': 'Free confidential emotional support available 24 hours a day',
+            'url': 'https://www.samaritans.org/uk',
+            'phone': '116 123',
+        },
+    ],
+    'UNITED_STATES': [
+        {
+            'name': 'National Maternal Mental Health Hotline',
+            'description': 'Free, confidential support for pregnant and postpartum mothers',
+            'url': 'https://mchb.hrsa.gov/national-maternal-mental-health-hotline',
+            'phone': '1-833-852-6262',
+        },
+        {
+            'name': 'Postpartum Support International',
+            'description': 'Support, resources and referrals for perinatal mental health',
+            'url': 'https://www.postpartum.net',
+            'phone': '1-800-944-4773',
+        },
+    ],
+    'CANADA': [
+        {
+            'name': 'Pacific Post Partum Support Society',
+            'description': 'Support for postpartum depression and anxiety',
+            'url': 'https://www.postpartum.org',
+            'phone': '1-855-255-7999',
+        },
+        {
+            'name': 'Crisis line',
+            'description': '24/7 crisis support line',
+            'url': 'https://988lifeline.org',
+            'phone': '988',
+        },
+    ],
+    'AUSTRALIA': [
+        {
+            'name': 'PANDA',
+            'description': 'Perinatal Anxiety & Depression Australia support line',
+            'url': 'https://panda.org.au',
+            'phone': '1300 726 306',
+        },
+    ],
+}
